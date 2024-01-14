b307095bfc24afb43a02f439152b42d808cb7bd8
Soumik Mukherjee <mukherjeesoumik700@gmail.com>
2024-01-14 15:38:53 +0530

    Utils updated

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,34 @@
+
+// this is also same as next one..but this written with promise..
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+export { asyncHandler }
+
+
+
+
+
+
+// What happening in behind in higer order function...
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
+
+// (try catch wraper function) we will use this function in other
+// parts of code to avoid rewrite same thing
+
+// const asyncHandler = (func) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
