8f2506044507ed94c3c1462d3aa3780b1d9c151c
Soumik Mukherjee <mukherjeesoumik700@gmail.com>
2024-01-27 20:51:08 +0530

    Refactor video comment controller to improve readability and error handling

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,33 +1,51 @@
-import mongoose from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
 import { Comment } from "../models/comment.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { Video } from "../models/video.model.js";
 
 const getVideoComments = asyncHandler(async (req, res) => {
   //TODO: get all comments for a video
   const { videoId } = req.params;
   const { page = 1, limit = 10 } = req.query;
-  try {
-    const allComments = await Comment.aggregate([
-      {
-        $match: {
-          video: new mongoose.Types.ObjectId(videoId), // When matching the raw Video id to video id in Database
-        },
-      },
-      {
-        $skip: (page - 1) * limit,
-      },
-      {
-        $limit: parseInt(limit, 10),
-      },
-    ]);
-    return res
-      .status(200)
-      .json(new ApiResponse(200, { allComments }, "Success"));
-  } catch (e) {
-    throw new ApiError(400, e.message);
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "This video id is not valid");
   }
+
+  // find video in database
+  const video = await Video.findById(videoId);
+  if (!video) {
+    throw new ApiError(404, "video not found");
+  }
+
+  // match and finds all the comments
+  const aggregateComments = await Comment.aggregate([
+    {
+      $match: {
+        video: new mongoose.Types.ObjectId(videoId),
+      },
+    },
+  ]);
+
+  Comment.aggregatePaginate(aggregateComments, {
+    page,
+    limit,
+  })
+    .then((result) => {
+      return res
+        .status(201)
+        .json(
+          new ApiResponse(200, result, "VideoComments fetched  successfully!!")
+        );
+    })
+    .catch((error) => {
+      throw new ApiError(
+        500,
+        "something went wrong while fetching video Comments",
+        error
+      );
+    });
 });
 
 const addComment = asyncHandler(async (req, res) => {
@@ -55,21 +73,32 @@ const addComment = asyncHandler(async (req, res) => {
 const updateComment = asyncHandler(async (req, res) => {
   // TODO: update a comment
   try {
-    const { content } = req.body;
     const { commentId } = req.params;
-    if (!content) throw new ApiError(404, "Comment Required");
-    const updateComments = await Comment.findByIdAndUpdate(
+    if (!commentId?.trim() || !isValidObjectId(commentId)) {
+      throw new ApiError(400, "commentId is required or invalid");
+    }
+
+    const content = req.body?.content?.trim();
+    if (!content) {
+      throw new ApiError(400, "Comment text is required to update comment");
+    }
+
+    const comment = await Comment.findByIdAndUpdate(
       commentId,
       {
-        content: content,
+        $set: {
+          content,
+        },
       },
       { new: true }
     );
-    if (!updateComments) throw new ApiError(500, "Something went wrong");
-    return res
+    if (!comment) {
+      throw new ApiError(500, "Something went wrong while updating comment");
+    }
+
+    res
       .status(200)
-      .json(new ApiResponse(200, { updateComments }, "Success"));
-    
+      .json(new ApiResponse(200, comment, "Comment update success"));
   } catch (e) {
     throw new ApiError(400, e.message || "Not able to update a comment");
   }
@@ -77,6 +106,16 @@ const updateComment = asyncHandler(async (req, res) => {
 
 const deleteComment = asyncHandler(async (req, res) => {
   // TODO: delete a comment
+  try {
+    const { commentId } = req.params;
+    const deleteComments = await Comment.findByIdAndDelete(commentId);
+    if (!deleteComments) throw new ApiError(500, "Something went wrong");
+    return res
+      .status(200)
+      .json(new ApiResponse(200, { deleteComments }, "Success"));
+  } catch (e) {
+    throw new ApiError(400, e.message || "Not able to delete a comment");
+  }
 });
 
 export { getVideoComments, addComment, updateComment, deleteComment };
